5efe3765e8f69af8f922111d33d9a85fb46ca669
MollyZen <denis.saltikov@gmail.com>
2023-08-30 20:12:06 +0300

    added assignment service

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,11 +2,10 @@ package app
 
 import (
 	"avito_test_case/config"
-	"avito_test_case/internal/datastruct"
 	"avito_test_case/internal/repository"
+	"avito_test_case/internal/service"
 	"avito_test_case/pkg/database"
 	"avito_test_case/pkg/logger"
-	"context"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,14 +13,19 @@ import (
 
 func Run(cfg *config.Config) {
 	l := logger.NewZeroLog(cfg.Log.Level)
+
 	db := database.NewPostgres(cfg.Postgres, l)
+
+	//repos
+	usRep := repository.NewPostgresUserRepository(db, l)
 	segRep := repository.NewPostgresSegmentRepository(db, l)
-	_, err := segRep.Create(context.TODO(), datastruct.Segment{
-		Name: "TEST_SEG",
-	})
-	if err != nil {
-		l.Error("Error adding new segment: %s", err)
-	}
+	asRep := repository.NewPostgresAssignmentRepository(db, l)
+	hisRep := repository.NewPostgresHistoryRepository(db, l)
+
+	//services
+	service.NewAssignmentUseCase(usRep, segRep, asRep, hisRep, l)
+
+	//http
 
 	// Waiting signal
 	interrupt := make(chan os.Signal, 1)
